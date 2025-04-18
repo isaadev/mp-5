fa7652dc0859f9e62f329ca6ae9f3d03b1e65141
i <101498370+isaadev@users.noreply.github.com>
2025-04-18 18:21:10 -0400

    pls work

--- src/app/r/[alias]/route.ts
+++ src/app/r/[alias]/route.ts
@@ -4,17 +4,15 @@ import getCollection from "../../lib/db";
 
 export async function GET(
     request: Request,
-    { params }: { params: { alias: string } }
+    { params }: { params: Promise<{ alias: string }> }
 ) {
-    const alias = params.alias;
+    const alias = await params;
     const links = await getCollection("links");
 
     const foundLink = await links.findOne({ alias });
     if (!foundLink) {
         console.log(`Alias not found in DB: ${alias}`);
-        return new Response('That shortened link does not exist', { status: 404 });
+        return new Response('That trimmed link does not exist', { status: 404 });
     }
-
-    console.log(`Redirecting ${alias} to ${foundLink.url}`);
     redirect(foundLink.url);
 }
