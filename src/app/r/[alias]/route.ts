2a0bdc348c141d6b8838fc3f9cdbf2cbd6f7e226
i <101498370+isaadev@users.noreply.github.com>
2025-04-18 18:45:59 -0400

    worrrrrrk

--- src/app/r/[alias]/route.ts
+++ src/app/r/[alias]/route.ts
@@ -1,18 +1,22 @@
-// src/app/r/[alias]/route.ts
-import { redirect } from 'next/navigation';
+import { NextResponse } from 'next/server';
 import getCollection from "../../lib/db";
 
 export async function GET(
     request: Request,
     { params }: { params: Promise<{ alias: string }> }
 ) {
-    const alias = await params;
+    const { alias } = await params;
     const links = await getCollection("links");
-
+    
     const foundLink = await links.findOne({ alias });
+    
+
     if (!foundLink) {
-        console.log(`Alias not found in DB: ${alias}`);
-        return new Response('That trimmed link does not exist', { status: 404 });
+        return NextResponse.json(
+        { error: 'That trimmed link does not exist' },
+        { status: 404 }
+        );
     }
-    redirect(foundLink.url);
+
+    return NextResponse.redirect(foundLink.url);
 }
