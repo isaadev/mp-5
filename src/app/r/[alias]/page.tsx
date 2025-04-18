b509f904a5bebb512904f11fb29585c4e9638cd5
i <101498370+isaadev@users.noreply.github.com>
2025-04-18 18:10:31 -0400

    hopefully

--- src/app/r/[alias]/page.tsx
+++ src/app/r/[alias]/page.tsx
@@ -1,16 +1,20 @@
+// src/app/r/[alias]/route.ts
 import { redirect } from 'next/navigation';
 import getCollection from "../../lib/db";
 
-export default async function Page({
-    params
-}: {
-    params: { alias: string }
-}) {
+export async function GET(
+    request: Request,
+    { params }: { params: { alias: string } }
+) {
+    const alias = params.alias;
     const links = await getCollection("links");
 
-    const foundLink = await links.findOne({ alias: params.alias });
+    const foundLink = await links.findOne({ alias });
     if (!foundLink) {
-        return <div>Link not found</div>;
+        console.log(`Alias not found in DB: ${alias}`);
+        return new Response('That shortened link does not exist', { status: 404 });
     }
+
+    console.log(`Redirecting ${alias} to ${foundLink.url}`);
     redirect(foundLink.url);
 }
