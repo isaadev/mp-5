6edd1dd471aab8f5d417dbad1f62a70694147970
i <101498370+isaadev@users.noreply.github.com>
2025-04-18 18:53:20 -0400

    idk

--- src/app/r/[alias]/route.ts
+++ src/app/r/[alias]/route.ts
@@ -5,18 +5,24 @@ export async function GET(
     request: Request,
     { params }: { params: Promise<{ alias: string }> }
 ) {
-    const { alias } = await params;
-    const links = await getCollection("links");
-    
-    const foundLink = await links.findOne({ alias });
-    
+    try {
+        const { alias } = await params;
+        const links = await getCollection("links");
+        const foundLink = await links.findOne({ alias });
 
-    if (!foundLink) {
+        if (!foundLink) {
+            return NextResponse.json(
+                { error: 'Link not found' },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.redirect(foundLink.url, 307);
+    } catch (error) {
+        console.error('Error:', error);
         return NextResponse.json(
-        { error: 'That trimmed link does not exist' },
-        { status: 404 }
+            { error: 'server error' },
+            { status: 500, headers: { 'Content-Type': 'application/json' } }
         );
     }
-
-    return NextResponse.redirect(foundLink.url);
 }
